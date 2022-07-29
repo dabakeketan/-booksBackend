6761f9e46b75ccbfb1e1ab78f917576bca5b7b84
kdabake <kdabake@crexendo.com>
2022-07-29 13:51:39 +0530

    TT

--- index.js
+++ index.js
@@ -14,24 +14,24 @@ mongoose
 const bookRoute = require('./routes/book.routes')
 const app = express()
 app.use(bodyParser.json())
-// app.use(
-//     bodyParser.urlencoded({
-//         extended: false,
-//     }),
-// )
-// app.use(cors())
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    }),
+)
+app.use(cors())
 
-// app.use((req, res, next) => {
-//     console.log('at origin', req);
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader(
-//       'Access-Control-Allow-Headers',
-//       'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//     );
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+app.use((req, res, next) => {
+    console.log('at origin', req);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   
-//     next();
-//   });
+    next();
+  });
 // Static directory path
 // app.use(express.static(path.join(__dirname, 'dist/angular-mean-crud-tutorial')))
 // API root
